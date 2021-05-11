32a3c9f7417c1eadea23bd59816d3b055c5a50ee
Ryan Lewis <r2c2ryan@gmail.com>
2021-05-11 15:17:40 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://cef9cd3e9b864cea80ed2430b5351d8b@o611595.ingest.sentry.io/5759949",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
